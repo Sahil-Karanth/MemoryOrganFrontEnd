979e8b1793e2a1f661920acead79090453c9bac4
Sahil-Karanth <1paleking1@gmail.com>
2025-02-08 22:28:43 +0000

    put electron in a function (not used for bots)

--- main.js
+++ main.js
@@ -1,33 +1,39 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
 
-let win;
 
-function createWindow() {
-  win = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      nodeIntegration: true,
-      contextIsolation: false, // Set to true for better security in production
+function makeElectronApp() {
+
+    let win;
+    
+    function createWindow() {
+      win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+          nodeIntegration: true,
+          contextIsolation: false, // Set to true for better security in production
+        }
+      });
+    
+      // Initially load the first HTML file
+      win.loadFile('index.html');
+    
     }
-  });
-
-  // Initially load the first HTML file
-  win.loadFile('index.html');
-
+    
+    app.whenReady().then(() => {
+      createWindow();
+    
+      app.on('window-all-closed', () => {
+        if (process.platform !== 'darwin') app.quit();
+      });
+    });
+    
+    app.on('activate', () => {
+      if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
+      }
+    });
 }
 
-app.whenReady().then(() => {
-  createWindow();
-
-  app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') app.quit();
-  });
-});
-
-app.on('activate', () => {
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow();
-  }
-});
+makeElectronApp()
